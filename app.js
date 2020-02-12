7799079bbd6038812654366442c1f3ae12c6712a
thejenkdog <thejenkdog@gmail.com>
2020-02-12 13:46:32 -0800

    Define route in app; instruct app to listen on port 5000

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
 const express = require("express");
 const app = express();
 const port = 5000;
+
+app.get("/", (req, res) => res.status(200).json({msg: "hello world"}))
+app.listen(port, () => console.log(`Server is running on port ${port}`));
